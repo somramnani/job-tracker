31ab3c2f00e23772137241083af635a4e2eca7cb
somramnani <som.ramnani@gmail.com>
2025-01-06 12:53:42 -0500

    feat: add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome to your Job tracker!</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+/* Requirements
+Current Date - filled with the current date
+Url
+Job name
+Category
+Point of contact (optional)
+*/
+const Form = () => {
+  return (
+    <div>
+      <h1>Form</h1>
+    </div>
+  );
+};
+
+export default Form;
