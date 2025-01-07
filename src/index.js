43f9e8d785bff284daa557a2e0515186941ebcca
somramnani <som.ramnani@gmail.com>
2025-01-07 15:55:48 -0500

    feat: add Form component with inputs, validation & date

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <LocalizationProvider dateAdapter={AdapterDateFns}>
     <App />
-  </React.StrictMode>
+  </LocalizationProvider>
 );
-
-
