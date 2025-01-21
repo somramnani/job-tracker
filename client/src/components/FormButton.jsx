307fe476d640ce1b61052d43eb1893f407935188
somramnani <som.ramnani@gmail.com>
2025-01-21 11:11:03 -0500

    refactor(form-button): Add form button components and loading

---
+++ client/src/components/FormButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button, CircularProgress } from "@mui/material";
+
+const FormButton = ({
+  message,
+  icon,
+  onClick,
+  isLoadingSubmit = false,
+  color = "primary",
+  type = "submit",
+  disabled = false,
+}) => {
+  return (
+    <Button
+      variant="contained"
+      startIcon={icon}
+      type={type}
+      onClick={onClick}
+      color={color}
+      disabled={disabled || isLoadingSubmit}
+      fullWidth
+    >
+      {isLoadingSubmit ? (
+        <CircularProgress size={24} color="inherit" />
+      ) : (
+        message
+      )}
+    </Button>
+  );
+};
+
+export default FormButton;
