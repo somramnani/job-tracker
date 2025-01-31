73204953b6aef6822d377d76c274bbe150738164
somramnani <som.ramnani@gmail.com>
2025-01-30 21:48:21 -0500

    chore(snackbar): add snackbar tests

--- client/src/tests/components/Navbar.test.jsx
+++ client/src/tests/components/Navbar.test.jsx
@@ -7,11 +7,15 @@ jest.mock("../../hooks", () => ({
   useAuth: jest.fn(),
 }));
 
+<<<<<<< HEAD
 const renderNavbar = (mockUser) => {
   useAuth.mockReturnValue({
     user: mockUser,
     handleLogout: jest.fn(),
   });
+=======
+const renderNavbar = () => {
+>>>>>>> be63079 (chore(snackbar): add snackbar tests)
   render(
     <MemoryRouter>
       <Navbar />
@@ -21,8 +25,17 @@ const renderNavbar = (mockUser) => {
 
 describe("Navbar", () => {
   it("renders the Navbar", () => {
+<<<<<<< HEAD
     const mockUser = null;
     renderNavbar(mockUser);
+=======
+    useAuth.mockReturnValue({
+      user: null,
+      handleLogout: jest.fn(),
+    });
+
+    renderNavbar();
+>>>>>>> be63079 (chore(snackbar): add snackbar tests)
 
     const navbar = screen.getByTestId("navbar");
     expect(navbar).toBeInTheDocument();
@@ -31,7 +44,11 @@ describe("Navbar", () => {
   it("renders Logout button when a user is logged in", () => {
     const mockUser = { id: "1", name: "Som", email: "test@gmail.com" };
 
+<<<<<<< HEAD
     renderNavbar(mockUser);
+=======
+    renderNavbar();
+>>>>>>> be63079 (chore(snackbar): add snackbar tests)
 
     const logoutButton = screen.getByText("Logout");
     expect(logoutButton).toBeInTheDocument();
