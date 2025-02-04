0323d5a25bdbe4894e00cf482563359d2c4fd61b
somramnani <som.ramnani@gmail.com>
2025-02-04 18:21:37 -0500

    test(error-message): add tests for Error Message component

---
+++ client/src/tests/components/ErrorMessage.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { ErrorMessage } from "components";
+
+describe("Error Message", () => {
+  it("renders the Error message", () => {
+    render(<ErrorMessage fieldName="Company" isFieldNotFound={true} />);
+
+    const errorMessage = screen.getByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("does not render when isFieldNotFound is false", () => {
+    render(<ErrorMessage fieldName="Company" isFieldNotFound={false} />);
+
+    expect(screen.queryByTestId("error-message")).toBeNull();
+  });
+
+  it("displays the correct error message", () => {
+    render(<ErrorMessage fieldName="Company" isFieldNotFound={true} />);
+
+    expect(screen.getByText("✗ Company not found")).toBeInTheDocument();
+  });
+});
