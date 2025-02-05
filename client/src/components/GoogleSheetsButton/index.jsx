09f325333503d366ca8f636733a50498efd290d1
somramnani <som.ramnani@gmail.com>
2025-02-04 19:43:22 -0500

    tests(google-sheet-button): add testid for google sheet button component

--- client/src/components/GoogleSheetsButton/index.jsx
+++ client/src/components/GoogleSheetsButton/index.jsx
@@ -3,21 +3,23 @@ import { GoogleIcon } from "components/Icons/CustomIcons";
 
 const GoogleSheetsButton = () => {
   return (
-    <Button
-      variant="contained"
-      color="primary"
-      startIcon={<GoogleIcon />}
-      target="_blank"
-      href={process.env.REACT_APP_GOOGLE_SHEET}
-      fullWidth
-      style={{
-        backgroundColor: "#6dc00b",
-        color: "#fff",
-        textTransform: "none",
-      }}
-    >
-      GOOGLE SHEETS DOCUMENT
-    </Button>
+    <div data-testid="google-sheet-button">
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<GoogleIcon />}
+        target="_blank"
+        href={process.env.REACT_APP_GOOGLE_SHEET}
+        fullWidth
+        style={{
+          backgroundColor: "#6dc00b",
+          color: "#fff",
+          textTransform: "none",
+        }}
+      >
+        GOOGLE SHEETS DOCUMENT
+      </Button>
+    </div>
   );
 };
 
