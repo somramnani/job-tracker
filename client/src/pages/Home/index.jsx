5e4a93553e6ff4e7af5c40414f4159fe0b41a863
somramnani <som.ramnani@gmail.com>
2025-02-07 21:45:01 -0500

    fix(e2e-tests): update Home page test id

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,7 +5,11 @@ import { Navigate } from "react-router-dom";
 const Home = () => {
   const { user } = useAuth();
 
-  return user ? <Navigate to="/job-board" replace /> : <SignIn />;
+  return (
+    <div data-testid="home-page">
+      {user ? <Navigate to="/job-board" replace /> : <SignIn />}
+    </div>
+  );
 };
 
 export default Home;
