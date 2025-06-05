c88294e2aea464985296382ec9f50228781fb42e
somramnani <som.ramnani@gmail.com>
2025-06-05 15:52:40 -0400

    setup(database):update  db.js for tests

--- server/db.js
+++ server/db.js
@@ -1,16 +1,21 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+if (process.env.NODE_ENV === "test") {
+  module.exports = {
+    query: jest.fn(),
+  };
+} else {
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL connected!");
-});
+  db.connect((err) => {
+    if (err) throw err;
+    console.log("DB connected");
+  });
 
-module.exports = db;
+  module.exports = db;
+}
