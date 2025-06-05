57f69876ade3418afec8fe56b78846222e4e1d6e
somramnani <som.ramnani@gmail.com>
2025-06-05 15:44:30 -0400

    setup(database): remove db import from server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-const db = require("./db");
 
 const app = express();
 
