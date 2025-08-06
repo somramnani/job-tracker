1c1b51d6337ff5fd207ad0f28b3084551e32fd1c
somramnani <som.ramnani@gmail.com>
2025-08-06 12:07:42 -0400

    test(Navbar): add test for when a user is not logged

--- client/src/tests/unit/components/Navbar.test.jsx
+++ client/src/tests/unit/components/Navbar.test.jsx
@@ -1,7 +1,8 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { Navbar } from "components";
 import { mockAuth, mockSnackbar } from "tests/utils/mockHooks";
 import { render } from "tests/utils/customRender";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 jest.mock("hooks", () => ({
   useAuth: jest.fn(),
@@ -41,5 +42,30 @@ describe("Navbar Component", () => {
 
     const loginButton = screen.queryByText("Login");
     expect(loginButton).not.toBeInTheDocument();
+  });
+  it("should render Login button and redirect to '/' when clicked", () => {
+    mockAuth({
+      user: null,
+      handleLogout: jest.fn(),
+    });
+
+    const TestComponent = () => <div>Home Page</div>;
+
+    render(
+      <MemoryRouter initialEntries={[]}>
+        <Routes>
+          <Route path="/" element={<TestComponent />} />
+          <Route path="*" element={<Navbar />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const loginButton = screen.getByText("Login");
+    expect(loginButton).toBeInTheDocument();
+
+    fireEvent.click(loginButton);
+
+    const homePage = screen.getByText("Home Page");
+    expect(homePage).toBeInTheDocument();
   });
 });
